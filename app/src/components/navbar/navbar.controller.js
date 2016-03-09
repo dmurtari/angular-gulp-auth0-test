726cffea5a96d4bce12930527ce15e091e2108ed
Domenic Murtari <domenic.murtari@gmail.com>
2016-03-09 15:40:59 -0700

    Changed wording in menubar

--- app/src/components/navbar/navbar.controller.js
+++ app/src/components/navbar/navbar.controller.js
@@ -15,14 +15,6 @@ module.exports = function(mbuAuthService, auth) {
   vm.shouldShowLogout = function() {
     return auth.isAuthenticated && !!auth.profile;
   };
-
-  vm.userName = function() {
-    if (auth.profile.user_metadata.firstname) {
-      return auth.profile.user_metadata.firstname;
-    } else {
-      return auth.profile.email;
-    }
-  };
-
+  
   return vm;
 };
