1571b06be939d3965eae73cd1db475a6f16c791b
Domenic Murtari <domenic.murtari@gmail.com>
2016-03-10 10:26:52 -0700

    Removed old information from profile controller

--- app/src/components/profile/profile.view.controller.js
+++ app/src/components/profile/profile.view.controller.js
@@ -6,22 +6,5 @@ module.exports = function(store) {
   vm.profile = store.get('profile');
   vm.metadata = store.get('profile').user_metadata;
 
-  vm.chapter = function() {
-    if (vm.metadata.role === 'teacher') {
-      switch(vm.metadata.teacher.chapter) {
-        case 'gammatheta':
-          return 'Gamma Theta';
-        case 'alphaetaiota':
-          return 'Alpha Eta Iota';
-        case 'epsilonxi':
-          return 'Epsilon Xi';
-        case 'mupi':
-          return 'Mu Pi';
-        case 'unaffiliated':
-          return 'Other community member';
-      }
-    }
-  };
-
   return vm;
 };
